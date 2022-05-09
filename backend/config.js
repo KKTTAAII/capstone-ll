df2da23b2705e29490be1fa8046ea4273e7ae52e
KKTTAAII <krataias65@gmail.com>
2022-05-09 15:44:30 -0600

    exclude my-secret-key

--- backend/config.js
+++ backend/config.js
@@ -4,15 +4,15 @@
 
 require("dotenv").config();
 
-const SECRET_KEY = process.env.SECRET_KEY || "KKTTAAIIEVIE";
+const SECRET_KEY = process.env.SECRET_KEY || process.env.MY_SECRET_KEY;
 
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "petly_test"
-      : process.env.DATABASE_URL || "petly";
+  return process.env.NODE_ENV === "test"
+    ? "petly_test"
+    : process.env.DATABASE_URL || "petly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
