ffb526c4a41292b1ff9c98278ccb3304626024ba
KKTTAAII <krataias65@gmail.com>
2022-05-28 23:51:12 -0600

    requests are sent indefinitely

--- middleware/auth.js
+++ middleware/auth.js
@@ -34,6 +34,7 @@ function authenticateJWT(req, res, next) {
 
 function ensureLoggedIn(req, res, next) {
   try {
+    console.log("Ensure loggedin")
     if (!res.locals.user) throw new UnauthorizedError();
     return next();
   } catch (err) {
